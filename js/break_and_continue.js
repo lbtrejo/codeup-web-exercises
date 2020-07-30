ec30c0f2b95ba7119fbf5d02c75c3061e29381cc
Levi Trejo <levi.b.trejo@gmail.com>
2020-07-30 16:14:57 -0500

    Added break loop for full credit

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -14,11 +14,25 @@ function numberIsEven(x) {
     return (x % 2 === 0);
 }
 
-do {
-    var oddNumber = prompt("Please enter an odd number between 1 and 50: ");
-} while(numberInvalid(oddNumber) || numberOutsideRange(oddNumber) || numberIsEven(oddNumber));
+// My original do while loop, before I read that I had to use a break
+// do {
+//     var oddNumber = prompt("Please enter an odd number between 1 and 50: ");
+// } while(numberInvalid(oddNumber) || numberOutsideRange(oddNumber) || numberIsEven(oddNumber));
 
-console.log("Number to skip is: " + oddNumber);
+while (true) {
+    var oddNumber = prompt("Please enter an odd number between 1 and 50: ");
+    if (numberInvalid(oddNumber)) {
+        console.log("You did not enter a valid number, please try again")
+    } else if (numberOutsideRange(oddNumber)) {
+        console.log("You number is outside the allowed range, please try again")
+    } else if (numberIsEven(oddNumber)) {
+        console.log("Your number is even, please try again")
+    } else {
+        console.log("Number to skip is: " + oddNumber);
+        break;
+    }
+}
+
 console.log("");
 
 for (var i = 1 ; i < 50 ; i += 2) {
