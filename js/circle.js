dfac0c8359ee20125b1a4bb3624a589bfe52143a
Levi Trejo <levi.b.trejo@gmail.com>
2020-08-06 09:57:43 -0500

    Math exercise solutions

--- js/circle.js
+++ js/circle.js
@@ -8,17 +8,21 @@
         getArea: function () {
             // TODO: complete this method
             // hint: area = pi * radius^2
-
-            return; // TODO: return the proper value
+            let area = Math.PI * Math.pow(this.radius, 2);
+            return area; // TODO: return the proper value
         },
 
         logInfo: function (doRounding) {
             // TODO: complete this method.
-
+            if (doRounding) {
+                console.log("Rounded area of a circle with radius: " + this.radius + ", is: " + Math.round(this.getArea()));
+                return Math.round(this.getArea());
+            }
             // If doRounding is true, round the result to the nearest integer.
             // Otherwise, output the complete value
 
-            console.log("Area of a circle with radius: " + this.radius + ", is: ");
+            console.log("Area of a circle with radius: " + this.radius + ", is: " + this.getArea());
+            return this.getArea();
         }
     };
 
@@ -30,7 +34,7 @@
 
     console.log("=======================================================");
     // TODO: Change the radius of the circle to 5.
-
+    circle.radius = 5;
     // log info about the circle
     console.log("Raw circle information");
     circle.logInfo(false);
