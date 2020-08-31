03753a4d28d846edfff5c45c21e3bd4f098473cd
Levi Trejo <levi.b.trejo@gmail.com>
2020-08-31 14:53:56 -0500

    feat: single place map complete

--- js/mapbox-api-exercise.js
+++ js/mapbox-api-exercise.js
@@ -1,1 +1,23 @@
 "use strict";
+
+geocode("8910 Bandera Rd #305, San Antonio, TX 78250", mapboxToken)
+    .then(function(result){
+        console.log(result);
+        mapboxgl.accessToken = mapboxToken;
+        let map = new mapboxgl.Map({
+            container: 'map',
+            style: 'mapbox://styles/mapbox/dark-v10',
+            zoom: 8
+        });
+        map.flyTo({center: result});
+        let marker = new mapboxgl.Marker({color: '#6edf7a'})
+            .setLngLat(result)
+            .addTo(map);
+        let popup = new mapboxgl.Popup({anchor: 'top'})
+            // .setLngLat(result)
+            .setText("Smashin' Crab " + String.fromCodePoint(0x1F980))
+            .addTo(map);
+
+        marker.setPopup(popup);
+        marker.togglePopup(popup);
+    });
