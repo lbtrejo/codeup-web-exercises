23448e06f1af541b47ddf1dc6bd2d39c32a7cbf0
Levi Trejo <levi.b.trejo@gmail.com>
2020-09-11 11:54:56 -0500

    feat: exercise complete

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -2,4 +2,12 @@
 
 $(document).ready(function(){
     console.log("Document is ready")
+
+    $("#toggle-btn").click(function(){
+        $("dd").toggleClass("invisible-custom")
+    })
+
+    $(".faq-header").click(function(){
+        $(this).addClass( ["custom-peach-bg", "custom-dark"] );
+    })
 })
