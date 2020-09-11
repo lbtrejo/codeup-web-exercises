00c9bedd965700fced2181268a52a3219e121e92
Levi Trejo <levi.b.trejo@gmail.com>
2020-09-11 14:46:16 -0500

    feat: problems 1-4 solved

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -1,13 +1,24 @@
 "use strict";
 
-$(document).ready(function(){
+$(document).ready(function() {
     console.log("Document is ready")
 
-    $("#toggle-btn").click(function(){
+    $("#toggle-btn").click(function () {
         $("dd").toggleClass("invisible-custom")
     })
 
-    $(".faq-header").click(function(){
-        $(this).addClass( ["custom-peach-bg", "custom-dark"] );
+    $(".faq-header").click(function () {
+        $(this).addClass(["custom-peach-bg", "custom-dark"]);
+    })
+
+    $("#yellow-bg-btn").click(function(){
+       $("#park-one-facts").children().last().css("background-color", "lightyellow");
+       $("#park-two-facts").children().last().css("background-color", "lightyellow");
+       $("#park-three-facts").children().last().css("background-color", "lightyellow");
+    })
+
+    $("h3").click(function(){
+        $(this).next().children("li").css("font-weight", "bold");
+        console.log(event);
     })
 })
