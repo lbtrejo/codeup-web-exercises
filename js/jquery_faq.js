20d1ee2f56a3cbd5fda6bc9be6ecd0cca2581e01
Levi Trejo <levi.b.trejo@gmail.com>
2020-09-11 16:46:49 -0500

    feat: finished effects exercise

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -3,6 +3,9 @@
 $(document).ready(function() {
     console.log("Document is ready")
 
+    $("ul").hide();
+    $("#registration-container").hide();
+
     $("#toggle-btn").click(function () {
         $("dd").toggleClass("invisible-custom")
     })
@@ -19,6 +22,7 @@ $(document).ready(function() {
 
     $("h3").click(function(){
         $(this).next().children("li").css("font-weight", "bold");
+        $(this).next().slideToggle(1000);
         // better solution is to toggle a class for this, since it's easier to scale/manage
     })
 
@@ -26,4 +30,13 @@ $(document).ready(function() {
         $(this).parent().children("li").first().css("color", "blue");
         // better solution is to toggle a class for this, since it's easier to scale/manage
     })
+
+    $("#close-icon").click(function(){
+        $("#newsletter-container").fadeOut(750);
+    })
+
+    window.setTimeout(function(){
+        $("#registration-container").fadeIn(1500)
+    }, 8000)
+
 })
