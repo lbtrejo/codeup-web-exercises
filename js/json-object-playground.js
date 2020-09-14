82f76ffea1f60d9fb9a898e9caf068b727fa82a4
Levi Trejo <levi.b.trejo@gmail.com>
2020-09-14 11:59:52 -0500

    feat: done with JSON exercise

--- js/json-object-playground.js
+++ js/json-object-playground.js
@@ -4,20 +4,20 @@ let cohort;
 
 cohort = {
     name: "bash",
-    numberStudents: 2,
+    number_of_students: 2,
     location: "Dallas, Tx",
     students: [
         {
-        studentName: "Stacy R.",
-        DOB: "1990-01-24",
-        scholarship: true,
-        capstone: null
+            name: "Stacy R.",
+            dob: "1990-01-24",
+            scholarship: true,
+            capstone: null
         },
         {
-        studentName: "Juan M.",
-        DOB: "1992-05-13",
-        scholarship: false,
-        capstone: null
+            name: "Juan M.",
+            dob: "1992-05-13",
+            scholarship: false,
+            capstone: null
         }],
     remote: false,
     instructors: ["Fernando M.", "Douglas H"]
