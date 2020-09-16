639da41579eba717e35b255d63ec23d995a674cc
Levi Trejo <levi.b.trejo@gmail.com>
2020-09-16 11:38:27 -0500

    feat: getForecastData and formatForcastData functions complete

--- js/weatherapp.js
+++ js/weatherapp.js
@@ -1,7 +1,6 @@
 (function(){
     "use strict";
 
-    const weatherURL = 'http://api.openweathermap.org/data/2.5/weather';
     const onecallURL = 'https://api.openweathermap.org/data/2.5/onecall';
 
     function getCurrentData(){
@@ -36,49 +35,50 @@
                 feels: data.current.feels_like,
                 temp: data.current.temp
             };
-            // resultCurrent.date = current.current.dt;
-            console.log("inside: ", resultCurrent);
             return resultCurrent;
     }
 
     function getForecastData(){
-        let forecastData = [];
-
-        $.get(onecallURL, {
-            lat: 29.42,
-            lon: -98.49,
-            appid: owmKey,
-            units: "imperial",
-            exclude: "minutely, hourly, current"
-        }).done(function(forecast){
-            for (let i = 1; i < 5; i++){
-                let resultCurrent = {
-                    date: forecast.daily[i].dt,
-                    dew_point: forecast.daily[i].dew_point,
-                    humidity: forecast.daily[i].humidity,
-                    pressure: forecast.daily[i].pressure,
-                    description: forecast.daily[i].weather[0].description,
-                    icon_large: "http://openweathermap.org/img/wn/"+ forecast.daily[i].weather[0].icon +"@2x.png",
-                    icon_small: "http://openweathermap.org/img/wn/"+ forecast.daily[i].weather[0].icon +".png",
-                    maxtemp: forecast.daily[i].temp.max,
-                    mintemp: forecast.daily[i].temp.min
+        return new Promise((resolve) => {
+            $.ajax({
+                url: onecallURL,
+                type: 'GET',
+                data: {
+                    lat: 29.42,
+                    lon: -98.49,
+                    appid: owmKey,
+                    units: "imperial",
+                    exclude: "minutely, hourly, current"
+                },
+                success: function(data) {
+                    resolve(data)
                 }
-                forecastData.push(resultCurrent);
-            }
-            console.log(forecastData);
-            return forecastData;
+            })
         })
     }
 
+    function formatForecastData (data){
+        let resultForecast = [];
+        for (let i = 1; i<= 4; i++){
+            let dailyForecast = {
+                date: new Date(data.daily[i].dt * 1000).toDateString(),
+                dew_point: data.daily[i].dew_point,
+                humidity: data.daily[i].humidity,
+                pressure: data.daily[i].pressure,
+                description: data.daily[i].weather[0].description,
+                icon_large: "http://openweathermap.org/img/wn/"+ data.daily[i].weather[0].icon + "@2x.png",
+                icon_small: "http://openweathermap.org/img/wn/"+ data.daily[i].weather[0].icon + ".png",
+                maxtemp: data.daily[i].temp.max,
+                mintemp: data.daily[i].temp.min
+            }
+            resultForecast.push(dailyForecast);
+        }
+
+        return resultForecast;
+    }
+
     function buildCurrentCard(currentObject){
-        // $.get(weatherURL, {
-        //         q: "San Antonio, Tx, US",
-        //         appid: owmKey,
-        //         units: "imperial"
-        //     }).done(function(weather){
-        //         console.log(weather);
             let cardHTML = "";
-            // let iconURL = "http://openweathermap.org/img/wn/"+ weather.weather[0].icon +"@2x.png";
             cardHTML += "<div class='card m-2'>"
             cardHTML += "<div class='card-header bg-success'>Today's Weather</div>"
             cardHTML += "<img src='" + currentObject.icon_large + "' class='card-img-top img-fluid mx-auto' style='max-width: 50%; height: auto;' alt='weather icon'>";
@@ -97,26 +97,13 @@
         getCurrentData()
             .then((data) => {
                 let currentObject = formatCurrentData(data);
-                // console.log("current object: ", currentObject);
                 buildCurrentCard(currentObject);
             })
-
-        // buildCurrentCard(currentData);
-        // console.log("Run getCurrent Function: ")
-        // getCurrentData();
-
-        // console.log("Run getForecast Function: ")
-        // getForecastData();
-
-    //     $.get(onecallURL, {
-    //         lat: 29.42,
-    //         lon: -98.49,
-    //         appid: owmKey,
-    //         units: "imperial",
-    //         exclude: "minutely, hourly"
-    //     }).done(function(forecast){
-    //         console.log(forecast);
-    //     })
+        getForecastData()
+            .then((data) => {
+                let forecastArray = formatForecastData(data);
+                console.log("Forecast Array: ", forecastArray);
+            })
     })
 
 })();
