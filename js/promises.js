5aa574ab2994891cbea33ded5a6ebe2df660d00f
Levi Trejo <levi.b.trejo@gmail.com>
2020-09-21 14:26:28 -0500

    feat: promisese exercise complete, now with working data filtering logic

--- js/promises.js
+++ js/promises.js
@@ -2,32 +2,32 @@
 
 const ghbaseURL = 'https://api.github.com/users';
 let ghUser = 'lbtrejo'
-let resultObject = {};
 
 const userInfo = (username) => {
     let requestURL = ghbaseURL + `/${username}/events/public`
     fetch(requestURL, {headers: {'Authorization': `token ${ghToken}`}})
         .then(response => response.json()
             .then((data) => {
-                resultObject = {
-                    username: data[0].actor.display_login,
-                    date: data[0].created_at,
-                    sha: data[0].payload.commits[0].sha,
-                    message: data[0].payload.commits[0].message,
-                    repo: data[0].repo.name
-                };
-                console.log("Result from Data callback", resultObject);
-                return resultObject;
-    }))
+                let resultArray = [];
+                data.forEach((eventObject) => {
+                    if (eventObject.type === "PushEvent"){
+                        resultArray.push(eventObject)
+                    }
+                })
+
+                console.log("Original Data", data);
+                console.log("Result Array", resultArray)
+                console.log("Latest Commit Timestamp: ", resultArray[0].created_at)
+                return resultArray[0].created_at;
+            }))
         .catch((error) => console.log(error));
 }
 
-let loggingTimeout = setTimeout(() => {
-    console.log("Result object after delay: ", resultObject);
-}, 2000)
-
-console.log("initial Result object: ", resultObject);
 userInfo(ghUser);
-console.log(loggingTimeout);
+
+$("#submitBtn").click(function(){
+    console.log($("#userInput").val());
+    userInfo($("#userInput").val());
+})
 
 
